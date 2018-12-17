4619b6dbb2b1d9e9f720fb8063442b2d80e60d51
Andy Luong <aluong@atpco.net>
2018-12-17 16:16:09 -0500

    - finish routing module

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -14,14 +14,19 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id']; // + converts string id from urll to number
-    this.server = this.serversService.getServer(id);
-    this.route.params
-      .subscribe(
-        (params: Params) => {
-          this.server = this.serversService.getServer(+params['id']);
-        }
-      );
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.server = data['server']; // data value must match resolve object in router
+      }
+    );
+    // const id = +this.route.snapshot.params['id']; // + converts string id from urll to number
+    // this.server = this.serversService.getServer(id);
+    // this.route.params
+    //   .subscribe(
+    //     (params: Params) => {
+    //       this.server = this.serversService.getServer(+params['id']);
+    //     }
+    //   );
   }
 
   onEdit() {
