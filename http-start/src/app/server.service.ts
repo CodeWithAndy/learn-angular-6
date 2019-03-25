f252da1e578a7f0bee19ce627e48a184deb217c8
Andy Luong <aluong@atpco.net>
2019-03-25 17:08:49 -0400

    - comments for rxjs 6+ and rxjs-compat

--- http-start/src/app/server.service.ts
+++ http-start/src/app/server.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http, Response } from '@angular/http';
+// RXJS 6+ SYNTAX
 import 'rxjs/RX';
 import { Observable } from 'rxjs/RX';
+// RXJS-COMPAT SYNTAX
+import { map } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class ServerService {
-    url = 'https://udemy-ng-http-7f66e.firebaseio.com/data';
-    constructor(private http: Http) {}
+    // url = 'https://udemy-ng-http-7f66e.firebaseio.com/data.json';
+    url = 'https://udemy-ng-http-7f66e.firebaseio.com/data'; // TEST ERROR
+    constructor(private http: Http) { }
     storeServers(servers: any[]) {
         // const headers = new Headers({'Content-Type': 'application/json'});
         // return this.http.post(this.url, servers, {headers: headers});
-        const headers = new Headers({'Content-Type': 'application/json'});
-        return this.http.put(this.url, servers, {headers: headers});
+        const headers = new Headers({ 'Content-Type': 'application/json' });
+        return this.http.put(this.url, servers, { headers: headers });
     }
     getServers() {
         return this.http.get(this.url)
+            // RXJS 6+ SYNTAX
             .map(
                 (response: Response) => {
                     const data = response.json();
@@ -24,11 +31,26 @@ export class ServerService {
                     return data;
                 }
             )
-            .catch(
-                (error: Response) => {
-                    return Observable.throw('Something went wrong');
-                }
-            );
+            // RXJS-COMPAT SYNTAX
+            // .pipe(
+            //     map(
+            //         (response: Response) => {
+            //             const data = response.json();
+            //             for (const server of data) {
+            //                 server.name = 'FETCHED_' + server.name;
+            //             }
+            //             return data;
+            //         }),
+            //     catchError(error => {
+            //         return throwError('Something went wrong');
+            //     })
+            // );
+        // RXJS 6+ SYNTAX
+        .catch(
+            (error: Response) => {
+                return Observable.throw('Something went wrong');
+            }
+        );
     }
 
     getAppName() {
