b0aeb4c1272a53de1cdd583c0448c56c54d75500
Andy Luong <ALuong@atpco.net>
2019-09-23 10:54:12 -0400

    - before grid update

--- fuse-v8.1.2-seed/src/app/main/marketview/snackbar.component.ts
+++ fuse-v8.1.2-seed/src/app/main/marketview/snackbar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'snack-bar-component-example-snack',
     template: `<div class="example-pizza-party">
-                    <mat-icon>save</mat-icon><span class="p-8">Save Snackbar Example</span>
+                    <mat-icon>save</mat-icon><span class="p-8">Save Successful</span>
                 </div>`,
     styles: [`
       .example-pizza-party mat-icon {
